9d99f6cc2ded9bf2fe65dbadb2d0e70139d992eb
PerkeleFan626 <ssebbe580@gmail.com>
2026-05-28 20:27:59 +1200

    Added more stuff

--- Headers/UserManager.h
+++ Headers/UserManager.h
@@ -10,4 +10,6 @@ public:
     UserManager(DataManager& manager);
 
     bool find_user_by_id(int searchId, PublicUser& foundUser);
+    bool find_user_by_name(const string& searchName, PublicUser& foundUser);
+    void print_all_users();
 };
--- UserManager.cpp
+++ UserManager.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
+// ============================================================================
+// 1. CONSTRUCTOR & INITIALIZATION
+// ============================================================================
+
 UserManager::UserManager(DataManager& manager) : dbManager(manager){}
+
+// ============================================================================
+// 2. PUBLIC USER OPERATIONS (CRUD & LOOKUPS)
+// ============================================================================
 
 bool UserManager::find_user_by_id(int searchId, PublicUser& foundUser) {
 
@@ -20,4 +28,36 @@ bool UserManager::find_user_by_id(int searchId, PublicUser& foundUser) {
 
     cout << "Error: User with ID" << searchId << " " << "could not be found. Enter another user ID \n";
     return false;
+}
+
+bool UserManager::find_user_by_name(const string& searchName, PublicUser& foundUser) {
+    vector<PublicUser> allUsers = dbManager.get_all_public_users();
+
+    for (const auto& user : allUsers) {
+        if (user.userName == searchName) {
+            foundUser = user;
+            return true;
+        }
+    }
+
+    cout << "Error: User with username " << searchName << " could not be found.\n";
+    return false;
+}
+
+void UserManager::print_all_users() {
+    vector<PublicUser> allUsers = dbManager.get_all_public_users();
+
+    if (allUsers.empty()) {
+        cout << "Database error: Found no users" << endl;
+        return;
+    }
+
+    for (const auto& user : allUsers) {
+        cout << user.uniqueId << " | "
+             << user.userName << " | "
+             << user.password << " | "
+             << user.email << " | "
+             << user.joiningDate << " | NZD "
+             << user.totalBill << endl;
+    }
 }
